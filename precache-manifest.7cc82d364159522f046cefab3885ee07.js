b725b5cfeb42f53d67b7c913b7a38e96ef29d620
kaori-sugawara <same1job@gmail.com>
2020-10-25 21:18:38 +0900

    Updates

---
+++ precache-manifest.7cc82d364159522f046cefab3885ee07.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "452cf72d7e007052a89a4943973a203d",
+    "url": "/eventpoint-calculation/index.html"
+  },
+  {
+    "revision": "280c5b8e0e15aa99705e",
+    "url": "/eventpoint-calculation/static/css/main.76a5ceb6.chunk.css"
+  },
+  {
+    "revision": "59c25c2c9bc203302547",
+    "url": "/eventpoint-calculation/static/js/2.a08baf51.chunk.js"
+  },
+  {
+    "revision": "5b571a43117d7de158db9e221eaa0225",
+    "url": "/eventpoint-calculation/static/js/2.a08baf51.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "280c5b8e0e15aa99705e",
+    "url": "/eventpoint-calculation/static/js/main.2121c1f0.chunk.js"
+  },
+  {
+    "revision": "4b22c53e17536975c08d",
+    "url": "/eventpoint-calculation/static/js/runtime-main.03ace438.js"
+  }
+]);
